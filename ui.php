fb60481d4fddce1e6f769fd7f3848469114c693e
Suraj Kushwah <surajkushwah6969@gmail.com>
2018-02-26 22:15:52 +0530

    Add files via upload

---
+++ ui.php
@@ -0,0 +1,48 @@
+<!doctype html>
+
+<html>
+	<head>
+		<title>UI Design</title>
+		
+		<style>
+			<?php  require_once('style.css');	?>
+		</style>
+	</head>
+	<body>
+		<div class="container main">
+			<div class="block1">
+				<div class="header">
+					<div class="favicon">
+						<label><a href="index.php"><span class="highlight">Bugs</span>writer</a></label>
+					</div>
+					<div class="nav">
+						<div class="navlist"><a href="#">About</a></div>
+						<div class="navlist"><a href="#">SignUp</a></div>
+						<div class="navlist"><a href="#">Login</a></div>
+						<div class="navlist"><a href="#">Contact</a></div>
+					</div>
+				</div>
+				<div class="block2">
+					<div class="text">
+						<h1>Hi, I am <span class="highlight">Suraj</span><br/>Welcome to my Website</h1>
+					</div>
+					<div class="buttons">
+						<button class="butt">Signup</button>
+						<button class="butt">Login</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container about">
+			<div class="aboutheading">
+				<h1>About</h1>
+			</div>	
+			<div class="myPicture">
+				
+			</div>
+			<div class="abouttext">
+				
+			</div>
+		</div>
+	</body>
+</html>
