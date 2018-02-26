3658eb5eef3e0eff91bf7f11a8d0378f3d087cf2
Suraj Kushwah <surajkushwah6969@gmail.com>
2018-02-26 22:14:16 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
+	
+	$title = "Welcome to Facebook";
 
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);	
-
-	include_once("functions.php");
-	session_start();
-
+	include_once('header.php');
 	
 	if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	
@@ -21,34 +17,33 @@
 	}
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Home | Bugswriter</title>
-</head>
-<body>
-	<h1>WELCOME TO SITENAME</h1>
-	<hr/>
 
-		<h2>Signup Form</h2>
-		<form method="POST" action="index.php" autocomplete="off">
-			<input type="text" name="username" placeholder="USERNAME"><br>
-			<input type="text" name="first_name" placeholder="FIRST NAME"><br>
-			<input type="text" name="last_name" placeholder="LAST NAME"><br>
-			<input type="text" name="email" placeholder="EMAIL ADDRESS"><br>
-			<input type="password" name="password" placeholder="PASSWORD"><br>
-			<input type="radio" name="gender" value="M" checked> Male
-	  		<input type="radio" name="gender" value="F"> Female<br>
-	  		<input type="submit" name="signup_submit" value="Register me">
-		</form>
 
-		<h2>Login Form</h2>
-		<form method="POST" action="index.php" autocomplete="off">
-			<input type="text" name="username" placeholder="USERNAME"><br>
-			<input type="password" name="password" placeholder="PASSWORD"><br>
-			<input type="submit" name="login_submit" value="Log in">
-		</form>
+	<?php if(!$check){ ?>
 	
+			<h2>Signup Form</h2>
+			<form method="POST" action="index.php" autocomplete="off">
+				<input type="text" name="username" placeholder="USERNAME"><br>
+				<input type="text" name="first_name" placeholder="FIRST NAME"><br>
+				<input type="text" name="last_name" placeholder="LAST NAME"><br>
+				<input type="text" name="email" placeholder="EMAIL ADDRESS"><br>
+				<input type="password" name="password" placeholder="PASSWORD"><br>
+				<input type="radio" name="gender" value="M" checked> Male
+		  		<input type="radio" name="gender" value="F"> Female<br>
+		  		<input type="submit" name="signup_submit" value="Register me">
+			</form>
 
-</body>
-</html>
+			<h2>Login Form</h2>
+			<form method="POST" action="index.php" autocomplete="off">
+				<input type="text" name="username" placeholder="USERNAME"><br>
+				<input type="password" name="password" placeholder="PASSWORD"><br>
+				<input type="submit" name="login_submit" value="Log in">
+			</form>
+	<?php }else{
+
+		//include_once('feed.php');
+		echo "You are logged in. services is yet building. Soon available :) ";
+	}
+	?>
+	
+<?php include_once('footer.php'); ?>
